1fa9ba5162a31910de8b61d30c66266826fefd55
aicherish <aicherish0320@163.com>
2022-08-22 07:53:39 +0800

    vitejs 初始化使用上

---
+++ vite-lesson/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img src="./assets/lisa2.jpg" alt="lisa" />
+    <HelloWorld msg="Vue3 + Vite" />
+    <img :src="img" />
+    <div class="box"></div>
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import img from './assets/lisa2.jpg'
+</script>
+
+<style>
+.box {
+  width: 100px;
+  height: 100px;
+  background-image: url(./assets/lisa2.jpg);
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+</style>
---
+++ vite-lesson/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>HelloWorld</div>
+</template>
+
+<script setup>
+defineProps({
+  msg: String
+})
+</script>
+
+<style lang="scss" scoped></style>
