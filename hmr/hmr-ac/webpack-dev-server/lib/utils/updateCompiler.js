ec3698d4002990ce22b538f4d758b2e565a872ba
aicherish <aicherish0320@163.com>
2022-09-20 07:44:08 +0800

    hmr 上

---
+++ hmr/hmr-ac/webpack-dev-server/lib/utils/updateCompiler.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+function updateCompiler(compiler) {
+  const options = compiler.options
+  // 它就是浏览器 websocket 客户端
+  compiler.entry.main.import.unshift(
+    require.resolve('../../client/index.js')
+  )
+  // 它用来在浏览器监听发射的事件，进行后续的热更新逻辑
+  compiler.entry.main.import.unshift(require.resolve('../../../webpack/hot/dev-server.js'))
+
+  console.log('compiler.entry >>> ', compiler.entry)
+
+  compiler.hooks.entryOption.call(options.context, config.entry)
+  
+
+}
+
+module.exports = updateCompiler
